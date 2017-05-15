812568c4cec25aafea2be8e957a346b6813a9062
Denzel <pressey90@gmail.com>
2017-05-14 23:44:46 -0400

    added fonts, boostrap layout, and copy

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Datelicious - Profile</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="container">
+	<div class="header">
+		<a href="http://datelicious.com.au/" target="_blank"><img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690389-228234-612x173-newlogodatelicious.png"></a>
+	</div>
+
+	<div class="row first-cta">
+		<div class="col-md-6">
+			<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690399-235310-958x458-hero1.jpg">
+		</div>
+
+		<div class="col-md-6">
+			<h1>Get Your Online Dating Profile</h1>
+			<p>Build an online dating profile that will get you discovered
+			and helps you connect with the right potential partners.
+			</p>
+			<button type="button" class="btn btn-danger btn-lg">Show Me Profile Plans!</button>
+		</div>
+	</div>
+
+	<div class="row intro-copy">
+		<div class="col-md-12">
+			<h2>Headline Two</h2>
+
+			<p>Your dating profile is the key to meeting the right partner for you; however, no matter who you are or what you do, it’s not easy to write about yourself and then add it to a dating website.</p>
+			
+			<p>You may be missing potential matches if your online dating profile doesn’t detail what you have to offer.</p>
+			
+			<p>It’s time to transform your underperforming online dating profile or, if you’re new to the game, take the leap into online dating. Turn online dating into a tool that highlights your fun side, who you are, and why a potential match should look no further than the profile right in front of them (and click to get in touch).</p>
+			
+			<p>The good news is that you don’t have to go it alone. We will work with you to create a dynamic profile that impresses potential matches, emphasises your qualities and outlines what you are looking for in a partner.</p>
+		</div>
+	</div>
+	<div class="row three-steps">
+		<h2>3 Easy Steps to an Impressive Profile</h2>
+
+		<ol class="custom-counter">
+			<li class="col-md-4">
+				<h3>Choose Your Plans</h3>
+				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690396-236292-100x100-0110TargetArrow100.png">
+				<p>Choose a plan from the four profile services detailed below.Note: the first package is do-it-yourself; therefore, you will receive a downloadable file with instructions after purchase.</p>	
+			</li>
+
+			<li class="col-md-4">
+				<h3>Get Started</h3>
+				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690393-235270-100x100-0007Chat100.png">
+				<p>After your purchase you will receive an email with instructions on how to get started which includes a questionnaire that assists us in learning about you.</p>	
+			</li>
+			
+			<li class="col-md-4">
+				<h3>Your Unique Online Dating Profile</h3>
+				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690394-235272-100x100-0065Speedometer100.png">
+				<p>Once we have received your questionnaire our team gets busy writing your online dating profile. We will deliver a stand out online dating profile within four business days.</p>				
+			</li>
+		</ol>
+
+	</div>
+	<div class="row plans">
+		
+	</div>
+	<div class="row last-cta">
+		
+	</div>
+</body>
+</html>
