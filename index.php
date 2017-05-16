3162f1c73f88970da27a7d30ca8687749143f638
Denzel <pressey90@gmail.com>
2017-05-16 07:46:43 -0400

    updated layout

--- index.php
+++ index.php
@@ -5,22 +5,26 @@
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body class="container">
+<body class="body-container">
 	<div class="header">
-		<a href="http://datelicious.com.au/" target="_blank"><img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690389-228234-612x173-newlogodatelicious.png"></a>
+			<a href="http://datelicious.com.au/" target="_blank"><img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690389-228234-612x173-newlogodatelicious.png"></a>
 	</div>
 
 	<div class="row first-cta">
-		<div class="col-md-6">
-			<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690399-235310-958x458-hero1.jpg">
-		</div>
+		<div class="shell">
+			<div class="col-md-6">
+				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690399-235310-958x458-hero1.jpg">
+			</div>
 
-		<div class="col-md-6">
-			<h1>Get Your Online Dating Profile</h1>
-			<p>Build an online dating profile that will get you discovered
-			and helps you connect with the right potential partners.
-			</p>
-			<button type="button" class="btn btn-danger btn-lg">Show Me Profile Plans!</button>
+			<div class="col-md-6">
+				<h1>Get Your Online Dating Profile</h1>
+				<p>Build an online dating profile that will get you discovered
+				and helps you connect with the right potential partners.
+				</p>
+				<button type="button" class="btn btn-danger btn-lg">Show Me Profile Plans!</button>
+			</div>
+
+			<div class="clear"></div>
 		</div>
 	</div>
 
@@ -40,25 +44,29 @@
 	<div class="row three-steps">
 		<h2>3 Easy Steps to an Impressive Profile</h2>
 
-		<ol class="custom-counter">
-			<li class="col-md-4">
-				<h3>Choose Your Plans</h3>
-				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690396-236292-100x100-0110TargetArrow100.png">
-				<p>Choose a plan from the four profile services detailed below.Note: the first package is do-it-yourself; therefore, you will receive a downloadable file with instructions after purchase.</p>	
-			</li>
+		<div class="shell">
+			<ol class="custom-counter">
+				<li class="col-md-4">
+					<h3>Choose Your Plans</h3>
+					<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690396-236292-100x100-0110TargetArrow100.png">
+					<p>Choose a plan from the four profile services detailed below.Note: the first package is do-it-yourself; therefore, you will receive a downloadable file with instructions after purchase.</p>	
+				</li>
 
-			<li class="col-md-4">
-				<h3>Get Started</h3>
-				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690393-235270-100x100-0007Chat100.png">
-				<p>After your purchase you will receive an email with instructions on how to get started which includes a questionnaire that assists us in learning about you.</p>	
-			</li>
-			
-			<li class="col-md-4">
-				<h3>Your Unique Online Dating Profile</h3>
-				<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690394-235272-100x100-0065Speedometer100.png">
-				<p>Once we have received your questionnaire our team gets busy writing your online dating profile. We will deliver a stand out online dating profile within four business days.</p>				
-			</li>
-		</ol>
+				<li class="col-md-4">
+					<h3>Get Started</h3>
+					<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690393-235270-100x100-0007Chat100.png">
+					<p>After your purchase you will receive an email with instructions on how to get started which includes a questionnaire that assists us in learning about you.</p>	
+				</li>
+				
+				<li class="col-md-4">
+					<h3>Your Unique Online Dating Profile</h3>
+					<img src="//v.fastcdn.co/t/e03b78b3/6c03a032/1470690394-235272-100x100-0065Speedometer100.png">
+					<p>Once we have received your questionnaire our team gets busy writing your online dating profile. We will deliver a stand out online dating profile within four business days.</p>				
+				</li>
+			</ol>
+
+			<div class="clear"></div>
+		</div>
 
 	</div>
 	<div class="row plans">
@@ -157,15 +165,21 @@
 	</div>
 	
 	<div class="row last-cta">
-		
-		<h4>If you are looking for the real deal and want to get to the DATELICIOUS part of dating - let us help you land those dates and get you there sooner.</h4>	
+		<div class="shell">
+			<h3>If you are looking for the real deal and want to get to the DATELICIOUS part of dating <br> - let us help you land those dates and get you there sooner.</h3>	
 
-		<button class="btn btn-danger btn-lg">Buy Now - All 4 packages links to purchase with prices</button>
+			<button class="btn btn-danger btn-lg">Buy Now - All 4 packages links to purchase with prices</button>
+
+			<div class="clear"></div>
+		</div>
 	</div>
 
 	<div class="footer">
-		<a class="faq" href="" target="_blank">F.A.Q</a>
-		<a class="terms-and-conditions" href="" target="_blank">Terms and Conditions</a>
+		<div class="shell">
+			<a class="faq" href="" target="_blank">F.A.Q</a>
+			<a class="terms-and-conditions" href="" target="_blank">Terms and Conditions</a>
+			<div class="clear"></div>
+		</div>
 	</div>
 </body>
 </html>
